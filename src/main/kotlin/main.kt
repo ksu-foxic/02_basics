e624c1a1be7c19df8f53c71e812ca59db211c88e
Ksenia Mitina <kseniamitina1985@gmail.com>
2025-09-14 10:53:37 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,38 @@
+fun main() {
+    //Задача 1
+    val amount = 15_000
+    val commission = 0.0075
+
+    val totalCommission = amount * commission
+    val result = if (totalCommission > 35) totalCommission else 35
+
+    println("Размер комиссии $result руб.")
+    println()
+
+    //Задача 2
+    val likes = 121
+    val people: String = if (likes == 1 || likes % 10 == 1) "человеку" else "людям"
+
+    println("Понравилось $likes $people")
+    println()
+
+    //Задача 3
+    val isRegular: Boolean = false
+    val cost = 13_000
+    val discount100 = 100
+    val discount5Per = 0.95
+
+    val resultCost: Double
+    if (1000 < cost && cost < 10001) {
+        resultCost = (cost - discount100).toDouble()
+    } else if (cost > 10000) {
+        resultCost = cost * discount5Per
+    } else resultCost = cost.toDouble()
+
+    val finalCost: Double
+    if (isRegular) {
+        finalCost = resultCost * 0.99
+    } else finalCost = resultCost
+
+    println("Стоимость покупки $finalCost руб.")
+}

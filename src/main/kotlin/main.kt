2ce768da173c69bb7e90ea6deec92eb13752d22c
Ksenia Mitina <kseniamitina1985@gmail.com>
2025-09-21 11:41:59 +0300

    change main.kt task2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,8 +10,8 @@ fun main() {
     println()
 
     //Задача 2
-    val likes = 161
-    val people: String = if (likes % 10 == 1 && likes != 11) "человеку" else "людям"
+    val likes = 211
+    val people: String = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
 
     println("Понравилось $likes $people")
     println()
